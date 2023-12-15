f4aaf8b6665537e3a70c63ba9ee5bb6efc2287d7
nguyentrungtin <tin.nguyentrung.tpv@one-line.com>
2023-12-15 16:34:27 +0700

    feat: Init

---
+++ main.js
@@ -0,0 +1,78 @@
+const puppeteer = require('puppeteer');
+const nodemailer = require('nodemailer');
+
+// Set up your email transport configuration
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'justin.tqdm@gmail.com',    // Replace with your email
+        pass: 'opavgreboldayxkx'      // Replace with your email password or an app-specific password
+    }
+});
+
+// Function to send an email with error details
+async function sendErrorEmail(error) {
+    const mailOptions = {
+        from: 'justin.tqdm@gmail.com',    // Replace with your email
+        to: 'justin.tqdm@gmail.com',  // Replace with the recipient's email
+        subject: 'Punch Failed!',
+        text: `An error occurred:\n\n${error.stack}`
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log('Error email sent successfully.');
+    } catch (sendError) {
+        console.error('Error sending email:', sendError);
+    }
+}
+async function randomDelay() {
+    const minutes = Math.floor(Math.random() * 5) + 1; // Generate a random number between 1 and 5
+    const milliseconds = minutes * 60 * 1000; // Convert minutes to milliseconds
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+
+const main = async () => {
+  try {
+    const username = 'tinnt'
+    const password = '@1111'
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+
+    // Set screen size
+    await page.setViewport({width: 1080, height: 1024});
+
+    // Navigate the page to a URL
+    await page.goto('https://b1lueprint.cyberlogitec.com.vn/');
+
+    // Type into search box
+    await page.type('#username', username);
+    await page.type('#password', password);
+
+    // Wait and click on first result
+    const searchResultSelector = '#submit-btn';
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
+
+    await page.goto('https://blueprint.cyberlogitec.com.vn/UI_TAT_028');
+
+    // Type into the text input with id "username" using XPath
+    const punchBtn = '//*[@id="checkInOutBody"]/div/div/div[1]/div[2]/div[1]/div/button';
+    await page.waitForXPath(punchBtn);
+    const button = await page.$x(punchBtn);
+    await randomDelay();
+    await button[0].click(punchBtn);
+
+    await browser.close();
+  } catch (error) {
+      // Handle the error
+      console.error('Error:', error);
+
+      // Send an email with error details
+      await sendErrorEmail(error);
+  }
+};
+// Wait for a moment (optional)
+main()
